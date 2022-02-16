5a1b2f2faa45c4db399fbd426c857eca683a0398
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-02-16 19:56:51 +0700

    Update absen-absen.js

--- plugins/absen-absen.js
+++ plugins/absen-absen.js
@@ -1,11 +1,11 @@
 let handler = async (m, { usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
-    if (!(id in conn.absen)) throw `_*Tidak ada absen berlangsung digrup ini!*_\n\n*${usedPrefix}mulaiabsen* - untuk memulai absen`
+    if (!(id in conn.absen)) throw `_*Tidak Ada Sesi Absensi Yang Sedang Berlangsung Digrup Ini!*_\n\n*${usedPrefix}mulaiabsen* - Untuk Memulai Absen`
 
     let absen = conn.absen[id][1]
     const wasVote = absen.includes(m.sender)
-    if (wasVote) throw '*Kamu sudah absen!*'
+    if (wasVote) throw '*Kamu Sudah Absen -_*'
     absen.push(m.sender)
     m.reply(`Done!`)
     let d = new Date
@@ -16,18 +16,15 @@ let handler = async (m, { usedPrefix }) => {
     })
     let list = absen.map((v, i) => `│ ${i + 1}. @${v.split`@`[0]}`).join('\n')
     conn.reply(m.chat, `*「 ABSEN 」*
-
-Tanggal: ${date}
+Tanggal : ${date}
 ${conn.absen[id][2]}
-
-┌ *Yang sudah absen:*
+┌ *List :*
 │ 
 │ Total: ${absen.length}
 ${list}
 │ 
 └────
-
-`, m, { contextInfo: { mentionedJid: absen } })
+*「 ABSEN 」*`, m, { contextInfo: { mentionedJid: absen } })
 }
 handler.help = ['absen']
 handler.tags = ['absen']
